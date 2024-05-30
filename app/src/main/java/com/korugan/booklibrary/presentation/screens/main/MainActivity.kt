4b89b40e0ed7479e05ac4631281893872f402e10
Korugan32 <tahacansenel@gmail.com>
2024-05-30 15:59:24 +0300

    Moved to presentation/screens/main

--- app/src/main/java/com/korugan/booklibrary/presentation/screens/main/MainActivity.kt
+++ app/src/main/java/com/korugan/booklibrary/presentation/screens/main/MainActivity.kt
@@ -1,10 +1,10 @@
-package com.korugan.booklibrary
+package com.korugan.booklibrary.presentation.screens.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.korugan.booklibrary.ui.theme.BookLibraryTheme
+import com.korugan.booklibrary.presentation.theme.BookLibraryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

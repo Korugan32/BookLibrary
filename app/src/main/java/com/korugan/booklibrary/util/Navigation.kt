45810d626463ad699978dc650b2a6f793bfe2c04
Korugan32 <tahacansenel@gmail.com>
2024-05-31 02:29:39 +0300

    Create Navigation.kt

---
+++ app/src/main/java/com/korugan/booklibrary/util/Navigation.kt
@@ -0,0 +1,22 @@
+package com.korugan.booklibrary.util
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.korugan.booklibrary.presentation.screens.LoginScreen
+import com.korugan.booklibrary.presentation.screens.SignUpScreen
+
+@Composable
+fun Navigation(){
+    val navController = rememberNavController()
+    
+    NavHost(navController = navController, startDestination = "loginScreen"){
+        composable(route = "loginScreen"){
+            LoginScreen(navController)
+        }
+        composable(route = "signUpScreen"){
+            SignUpScreen(navController)
+        }
+    }
+}

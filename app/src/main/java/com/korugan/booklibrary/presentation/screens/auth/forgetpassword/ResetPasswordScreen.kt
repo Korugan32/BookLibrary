672ba6ff8388828591fe942e6fccb31b5b7d86c0
Korugan32 <tahacansenel@gmail.com>
2024-06-01 23:47:11 +0300

    Refactor Screens

--- app/src/main/java/com/korugan/booklibrary/presentation/screens/auth/forgetpassword/ResetPasswordScreen.kt
+++ app/src/main/java/com/korugan/booklibrary/presentation/screens/auth/forgetpassword/ResetPasswordScreen.kt
@@ -1,4 +1,4 @@
-package com.korugan.booklibrary.presentation.screens
+package com.korugan.booklibrary.presentation.screens.auth.forgetpassword
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -10,8 +10,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Check
-import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.Lock
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -33,94 +31,9 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.times
 import androidx.navigation.NavHostController
+import com.korugan.booklibrary.presentation.screens.auth.screenWidth
 import com.korugan.booklibrary.presentation.theme.Blue
 import com.korugan.booklibrary.presentation.theme.Purple
-
-@Composable
-fun FPasswordScreen(navController: NavHostController) {
-    val email = remember {
-        mutableStateOf("")
-    }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(brush = Brush.verticalGradient(colors = listOf(Blue, Purple)))
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(screenWidth() * 0.10.dp),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Column {
-                Text(text = "Forget Password", style = TextStyle(color = Color.White, fontSize = 30.sp, fontWeight = FontWeight.W500))
-                Text(text = "Enter your email for verification process we will send 4 digits code to your mail", color = Color.White)
-                Spacer(modifier = Modifier.padding(5.dp))
-                OutlinedTextField(
-                    value = email.value, onValueChange = { email.value = it }, modifier = Modifier
-                        .fillMaxWidth(),
-                    textStyle = TextStyle(color = Color.White),
-                    label = { Text(text = "Email", color = Color.White) },
-                    trailingIcon = { Icon(imageVector = Icons.Outlined.Email, contentDescription = "", tint = Color.White) }
-                )
-                Spacer(modifier = Modifier.padding(5.dp))
-                Button(
-                    onClick = { navController.navigate("passwordCodeVerification") },
-                    modifier = Modifier
-                        .width(screenWidth() * 0.56.dp)
-                        .align(alignment = Alignment.CenterHorizontally),
-                    colors = ButtonDefaults.buttonColors(containerColor = Blue),
-                ) {
-                    Text(text = "Send Code")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun CodeVerificationScreen(navController: NavHostController) {
-    val code = remember {
-        mutableStateOf("")
-    }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(brush = Brush.verticalGradient(colors = listOf(Blue, Purple)))
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(screenWidth() * 0.10.dp),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Column {
-                Text(text = "Enter Code", style = TextStyle(color = Color.White, fontSize = 30.sp, fontWeight = FontWeight.W500))
-                Text(text = "Enter your 4 digits code for reset your password", color = Color.White)
-                Spacer(modifier = Modifier.padding(5.dp))
-                OutlinedTextField(
-                    value = code.value, onValueChange = { code.value = it }, modifier = Modifier
-                        .fillMaxWidth(),
-                    maxLines = 4,
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                    textStyle = TextStyle(color = Color.White),
-                    label = { Text(text = "Code", color = Color.White) },
-                    trailingIcon = { Icon(imageVector = Icons.Outlined.Check, contentDescription = "", tint = Color.White) }
-                )
-                Spacer(modifier = Modifier.padding(5.dp))
-                Button(
-                    onClick = { navController.navigate("resetPassword") },
-                    modifier = Modifier
-                        .width(screenWidth() * 0.56.dp)
-                        .align(alignment = Alignment.CenterHorizontally),
-                    colors = ButtonDefaults.buttonColors(containerColor = Blue),
-                ) {
-                    Text(text = "Continue")
-                }
-            }
-        }
-    }
-}
 
 @Composable
 fun ResetPassword(navController: NavHostController) {

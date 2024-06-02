763adc936619a0ff9c34ce68e45827429e74dbb0
Korugan32 <tahacansenel@gmail.com>
2024-06-02 18:36:59 +0300

    Create BottomBar Component

---
+++ app/src/main/java/com/korugan/booklibrary/presentation/components/BottomBar.kt
@@ -0,0 +1,57 @@
+package com.korugan.booklibrary.presentation.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.CheckCircle
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.times
+import androidx.navigation.NavHostController
+import com.korugan.booklibrary.presentation.screens.auth.screenWidth
+
+@Composable
+fun BottomBar(navController: NavHostController) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(screenWidth() * 0.21.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        Icon(
+            imageVector = Icons.Outlined.FavoriteBorder,
+            contentDescription = "",
+            modifier = Modifier
+                .size(screenWidth()*0.1.dp)
+                .clickable { navController.navigate("mainFavorites") },
+            tint = Color.White,
+        )
+        Icon(
+            imageVector = Icons.Outlined.Info,
+            contentDescription = "",
+            modifier = Modifier
+                .size(screenWidth()*0.1.dp)
+                .clickable { navController.navigate("mainReading") },
+            tint = Color.White,
+        )
+        Icon(
+            imageVector = Icons.Outlined.CheckCircle,
+            contentDescription = "",
+            modifier = Modifier
+                .size(screenWidth()*0.1.dp)
+                .clickable { navController.navigate("mainRead") },
+            tint = Color.White,
+        )
+    }
+}

645f8e99bd623402bdd3921d852785713eae7e6a
Korugan32 <tahacansenel@gmail.com>
2024-06-02 14:39:20 +0300

    Create RegisterAuth

---
+++ app/src/main/java/com/korugan/booklibrary/data/RegisterAuth.kt
@@ -0,0 +1,37 @@
+package com.korugan.booklibrary.data
+
+import android.widget.Toast
+import androidx.navigation.NavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
+fun registerAuth(username: String, email: String, password: String, navController: NavController) {
+    if (username.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()){
+    FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+        .addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val userId = FirebaseAuth.getInstance().currentUser?.uid
+                val db = FirebaseFirestore.getInstance()
+                val user = hashMapOf(
+                    "username" to username,
+                    "email" to email
+                )
+                userId?.let {
+                    db.collection("users").document(it).set(user)
+                        .addOnFailureListener { e ->
+                            Toast.makeText(null,"Error : $e",Toast.LENGTH_SHORT).show()
+                        }
+                }
+                loginAuth(email,password,navController)
+            } else {
+                Toast.makeText(null,"Registration Failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }else if (username.contains(" ")){
+        Toast.makeText(null,"Username Incorrect", Toast.LENGTH_SHORT).show()
+    }else if (email.contains(" ")){
+        Toast.makeText(null,"Email Incorrect", Toast.LENGTH_SHORT).show()
+    }else if (password.contains(" ")) {
+        Toast.makeText(null, "Password Incorrect", Toast.LENGTH_SHORT).show()
+    }
+}

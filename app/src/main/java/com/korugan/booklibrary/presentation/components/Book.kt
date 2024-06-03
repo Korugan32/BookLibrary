004a725bfc71c6933a082294422c6cf5f5a84cd4
Korugan32 <tahacansenel@gmail.com>
2024-06-03 15:16:56 +0300

    Create Book Components

---
+++ app/src/main/java/com/korugan/booklibrary/presentation/components/Book.kt
@@ -0,0 +1,26 @@
+package com.korugan.booklibrary.presentation.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
+import com.korugan.booklibrary.util.BookData
+
+@Composable
+fun Book(book: BookData,navController: NavController) {
+        Column(Modifier.padding(horizontal = 8.dp, vertical = 4.dp)) {
+            Image(painter = rememberAsyncImagePainter(model = book.thumbnail), contentDescription = "", modifier = Modifier
+                .height(140.dp)
+                .width(130.dp).clickable {
+                    println(book.thumbnail)
+                navController.navigate("mainBookInfo" + "?bookTitle=${book.title}&bookAuthor=${book.author}&bookImage=${book.thumbnail}")
+                })
+        }
+}

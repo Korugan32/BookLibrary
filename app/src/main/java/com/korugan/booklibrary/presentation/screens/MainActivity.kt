dfceed838705af237ed1040e875abcd6d94b4bdc
Korugan32 <tahacansenel@gmail.com>
2024-06-03 15:17:33 +0300

    Update MainActivity.kt

--- app/src/main/java/com/korugan/booklibrary/presentation/screens/MainActivity.kt
+++ app/src/main/java/com/korugan/booklibrary/presentation/screens/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.korugan.booklibrary.presentation.screens
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import com.korugan.booklibrary.presentation.theme.BookLibraryTheme
 import com.korugan.booklibrary.util.Navigation
 
@@ -13,8 +18,24 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BookLibraryTheme {
+                val launcher = rememberLauncherForActivityResult(
+                    ActivityResultContracts.RequestPermission()
+                ) { isGranted ->
+                    if (isGranted) {
+                        Toast.makeText(this, "İzin verildi", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "İzin reddedildi", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                if (!hasInternetPermission()) {
+                    launcher.launch(Manifest.permission.INTERNET)
+                }
                 Navigation()
             }
         }
+    }
+
+    private fun hasInternetPermission(): Boolean {
+        return applicationContext.checkSelfPermission(Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED
     }
 }

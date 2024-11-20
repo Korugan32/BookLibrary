cae608fb4a2e7353121e075a30a141379c5c7971
Korugan32 <tahacansenel@gmail.com>
2024-11-20 12:23:01 +0300

    Update SearchUserScreen

--- app/src/main/java/com/korugan/booklibrary/presentation/screens/main/SearchUserScreen.kt
+++ app/src/main/java/com/korugan/booklibrary/presentation/screens/main/SearchUserScreen.kt
@@ -7,11 +7,11 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -33,9 +33,11 @@ fun SearchUser(navController: NavHostController) {
     val query = remember { mutableStateOf("") }
     val usersData = remember { mutableStateOf<List<UserData>>(emptyList()) }
 
-    searchUser(query.value) { users ->
-        users.forEach { user ->
-            usersData.value = listOf(user)
+    LaunchedEffect(key1 = query.value) {
+        searchUser(query.value) { users ->
+            users.forEach { user ->
+                usersData.value = listOf(user)
+            }
         }
     }
     Column(

80dc7e6076d9f978d8841a60e11af92a44048b7a
Korugan32 <tahacansenel@gmail.com>
2024-11-21 13:46:49 +0300

    Update User Component

--- app/src/main/java/com/korugan/booklibrary/presentation/components/User.kt
+++ app/src/main/java/com/korugan/booklibrary/presentation/components/User.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -16,6 +15,7 @@ import androidx.compose.material.icons.outlined.PersonRemoveAlt1
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -30,15 +30,18 @@ import com.korugan.booklibrary.util.UserData
 @Composable
 fun User(user:UserData,navController:NavHostController) {
     val isFriendIcon = remember { mutableStateOf(Icons.Outlined.PersonAddAlt) }
-    friendControl(user.id){
-        if (it){
-            isFriendIcon.value = Icons.Outlined.PersonRemoveAlt1
+
+    LaunchedEffect(Unit) {
+        friendControl(user.id){
+            if (it){
+                isFriendIcon.value = Icons.Outlined.PersonRemoveAlt1
+            }
         }
     }
+
     Row(
         modifier = Modifier
-            .fillMaxWidth()
-            .height(70.dp),
+            .fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
     ) {
         Icon(imageVector = Icons.Outlined.Person, contentDescription = "", tint = Color.White, modifier = Modifier.size(40.dp).clickable { navController.navigate("profileScreen"+"?userId=${user.id}&userName=${user.name}") })
@@ -47,6 +50,5 @@ fun User(user:UserData,navController:NavHostController) {
         Row (modifier = Modifier.fillMaxWidth(), Arrangement.End){
             Icon(imageVector = isFriendIcon.value, contentDescription = "", tint = Color.White,modifier = Modifier.size(40.dp).clickable { setFriend(user.id); navController.navigate("mainFavorites") })
         }
-
     }
 }
